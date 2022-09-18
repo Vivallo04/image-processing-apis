8fade6e5a0474bf0dc210410e49252a32102d4e8
Vivallo04 <andresvivallo4@gmail.com>
2022-09-17 23:38:05 -0600

    Added main OpenCV setup

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/core/cuda.hpp>
+#include "opencv2/imgproc.hpp"
+#include <opencv2/opencv.hpp>
+
+
+int import_image()
+{
+
+    std::string const image_path = "/home/vivallo/Development/Datos II/OpenCVPOC/assets/mai.png";
+    cv::Mat img = cv::imread(image_path);
+    if(img.empty())
+    {
+        std::cout<<"COULD NOT READ IMAGE"<<std::endl;
+        return 1;
+    }
+    imshow("Processed Image", img);
+    cv::waitKey(0);
+}
+
+int main()
+{
+    std::cout << "Hello, World!" << std::endl;
+    import_image();
+    return 0;
+}

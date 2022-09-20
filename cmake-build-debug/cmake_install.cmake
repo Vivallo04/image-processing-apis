9bf3cbea31e62607662dd09ddbd4cd0c0b7a5c72
Jose Vargas <tamiguisimo@gmail.com>
2022-09-19 20:49:26 -0600

    a .h archive was made in order to store all the methods of the filters. All the filters are now correctly made and functioning well

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/josev/CLionProjects/proyectoprueba
+# Install script for directory: /home/josev/CLionProjects/features-jvargas-imageAPI
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/josev/CLionProjects/proyectoprueba/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/josev/CLionProjects/features-jvargas-imageAPI/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")

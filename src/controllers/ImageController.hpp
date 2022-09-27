7a413a120fa12f884ce83bd71999f8baa524e6c8
Jose Vargas <tamiguisimo@gmail.com>
2022-09-27 00:17:41 -0600

    changes in all filters in order to modelate them for testing

--- src/controllers/ImageController.hpp
+++ src/controllers/ImageController.hpp
@@ -5,7 +5,7 @@ class ImageController
 {
 public:
     static cv::Mat GaussianBlur_Filter(const cv::Mat &img_orig, cv::Mat img_new, int width, int height);
-    static cv::Mat GrayScale_Filter(const cv::Mat &img_orig, cv::Mat img_new);
-    static cv::Mat BrightnessControl_Filter(const cv::Mat &img_orig, cv::Mat img_new,int contrast,int bright);
-    static cv::Mat GammaCorrection_Filter(const cv::Mat &img_orig, cv::Mat img_new,int gamma_cor);
+    static cv::Mat GrayScale_Filter(const cv::Mat &img_orig, cv::Mat img_new,int width,int height);
+    static cv::Mat BrightnessControl_Filter(const cv::Mat &img_orig, cv::Mat img_new,int delta_bright,int width,int height);
+    static cv::Mat GammaCorrection_Filter(const cv::Mat &img_orig, cv::Mat img_new,int gamma_cor, int width,int height);
 };

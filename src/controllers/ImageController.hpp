110a05b1bc18f8efb864ef11d944447511bd7bc9
Vivallo04 <andresvivallo4@gmail.com>
2022-10-22 21:07:55 -0600

    Add Testing with mocks

--- src/controllers/ImageController.hpp
+++ src/controllers/ImageController.hpp
@@ -4,13 +4,8 @@
 class ImageController
 {
 public:
-
-
-    int GaussianBlurFilter(const cv::Mat &img_orig, cv::Mat &img_new, int width, int height);
-
-    int GrayScaleFilter(const cv::Mat &img_orig, cv::Mat &img_new, int width, int height);
-
-    int BrightnessControlFilter(const cv::Mat &img_orig, cv::Mat &img_new, int delta_bright, int width, int height);
-
-    int GammaCorrectionFilter(const cv::Mat &img_orig, cv::Mat &img_new, int gamma_cor, int width, int height);
+    int static GaussianBlurFilter(const cv::Mat &img_orig, cv::Mat &img_new, int width, int height);
+    int static GrayScaleFilter(const cv::Mat &img_orig, cv::Mat &img_new, int width, int height);
+    int static BrightnessControlFilter(const cv::Mat &img_orig, cv::Mat &img_new, int delta_bright, int width, int height);
+    int static GammaCorrectionFilter(const cv::Mat &img_orig, cv::Mat &img_new, int gamma_cor, int width, int height);
 };
--- test/ImageControllerTest.cpp
+++ test/ImageControllerTest.cpp
@@ -4,69 +4,83 @@
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/core/cuda.hpp>
 
+#include <gmock/gmock-spec-builders.h>
 #include "gtest/gtest.h"
 
-#include ".././src/controllers/ImageController.hpp"
+#include "mocks/MockImageController.cpp"
 
+using ::testing::AtLeast;
 
 // Tests --------------------------------------------------------
-const std::string happy_path = "/home/vivallo/Development/Datos II/image-processing-apis/assets/naruto.png";
-TEST(GrayScaleFilterTest, Handlesemptyimage)
+TEST(BrightnessControlFilterTest, ChangesBrightness)
 {
     // Act
+    MockImageController mockImageController;
+    const std::string happy_path = "/home/vivallo/Development/Datos II/image-processing-apis/assets/mai.png";
+
     std::string const image_path = happy_path;
     cv::Mat img = cv::imread(image_path);
-    cv::Mat img_error;
     cv::Mat img_new;
 
-    // Arrange
-    auto sut = ImageController::GrayScaleFilter(img, img_new, 300, 300);
-
-    // Assert
-    EXPECT_EQ(sut, 0);
+    // Arrange + Assert
+    EXPECT_EQ(mockImageController.BrightnessControlFilter(img, img_new, 50, 300, 300), 0);
 }
 
-TEST(GaussianBlurFilterTest, Handlesemptyimage)
-{
-    //Act
-    std::string const image_path = happy_path;
-    cv::Mat img = cv::imread(image_path);
-    cv::Mat img_error;
-    cv::Mat img_new;
-
-    //Arrange
-    auto sut= ImageController::GaussianBlurFilter(img,img_new,300,300);
-
-    // Assert
-    EXPECT_EQ(sut, 0);
-}
-
-TEST(GammaCorrectionFilterTest, Handlesemptyimage)
+TEST(GaussianFilterTest, ChangesOpacity)
 {
     // Act
+    MockImageController mockImageController;
+    const std::string happy_path = "/home/vivallo/Development/Datos II/image-processing-apis/assets/mai.png";
+
     std::string const image_path = happy_path;
     cv::Mat img = cv::imread(image_path);
-    cv::Mat img_error;
     cv::Mat img_new;
 
-    // Arrange
-    auto sut= ImageController::GammaCorrectionFilter(img, img_new, 2.54, 300, 300);
-
-    // Assert
-    EXPECT_EQ(sut, 0);
+    // Arrange + Assert
+    EXPECT_EQ(mockImageController.GaussianBlurFilter(img, img_new, 300, 300), 0);
 }
 
-TEST(BrightnessControlFilterTest, Handlesemptyimage)
+TEST(GrayScaleFilterTest, ChangesToGrayScale)
 {
     // Act
+    MockImageController mockImageController;
+    const std::string happy_path = "/home/vivallo/Development/Datos II/image-processing-apis/assets/mai.png";
+
     std::string const image_path = happy_path;
     cv::Mat img = cv::imread(image_path);
-    cv::Mat img_error;
     cv::Mat img_new;
 
-    // Arrange
-    auto sut= ImageController::BrightnessControlFilter(img, img_new, 50, 300, 300);
+    // Arrange + Assert
+    EXPECT_EQ(mockImageController.GrayScaleFilter(img, img_new, 300, 300), 0);
+}
 
-    // Assert
-    EXPECT_EQ(sut, 0);
+TEST(GammaCorrectionFilterTest, ChangesGamma)
+{
+    // Act
+    MockImageController mockImageController;
+    const std::string happy_path = "/home/vivallo/Development/Datos II/image-processing-apis/assets/mai.png";
+
+    std::string const image_path = happy_path;
+    cv::Mat img = cv::imread(image_path);
+    cv::Mat img_new;
+
+    // Arrange + Assert
+    EXPECT_EQ(mockImageController.GammaCorrectionFilter(img, img_new, 50, 300, 300), 0);
+}
+
+TEST(AllFiltersTest, ChangesAllFilters)
+{
+    // Act
+    MockImageController mockImageController;
+    const std::string happy_path = "/home/vivallo/Development/Datos II/image-processing-apis/assets/mai.png";
+
+    std::string const image_path = happy_path;
+    cv::Mat img = cv::imread(image_path);
+    cv::Mat img_new;
+
+    // Arrange + Assert
+    EXPECT_EQ(mockImageController.GammaCorrectionFilter(img, img_new, 50, 300, 300), 0);
+    EXPECT_EQ(mockImageController.BrightnessControlFilter(img, img_new, 50, 300, 300), 0);
+    EXPECT_EQ(mockImageController.GaussianBlurFilter(img, img_new, 300, 300), 0);
+    EXPECT_EQ(mockImageController.GrayScaleFilter(img, img_new, 300, 300), 0);
 }
